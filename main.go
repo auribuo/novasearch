8f832c4ab9d75b676ff714600d05ff5ec9077e2d
Aurelio Buonomo <auribuo@gmail.com>
2023-03-12 13:36:01 +0100

    Added global swagger info

--- main.go
+++ main.go
@@ -5,6 +5,18 @@ import (
 	"github.com/spf13/cobra"
 )
 
+// @title           Novasearch API
+// @version         1.0
+// @description     Api specification for Novasearch API
+
+// @contact.name   auribuo
+// @contact.url    https://github.com/auribuo/novasearch/issues
+
+// @license.name  MIT
+// @license.url   https://opensource.org/licenses/MIT
+
+// @host      localhost:8080
+// @BasePath  /api
 func main() {
 	cobra.CheckErr(cmd.Execute())
 }
